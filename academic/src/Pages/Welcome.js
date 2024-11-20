0545f72473852e18f63d457241425537c3c8e0a7
Kanchana <thennakoon.kanchana@gmail.com>
2024-11-20 10:36:45 +0530

    created a dummy page

---
+++ academic/src/Pages/Welcome.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './Welcome.css';
+
+function Welcome () {
+    return(
+        <div>
+            <h1>Dilankata office eke nangi set weewaa...!</h1>
+        </div>
+    )
+}
+export default Welcome;
