17bcc0970c1d47746a2177bff4a030340a8891b8
Dilanka Silva <dilankasilva725@gmail.com>
2024-12-29 05:57:55 +0530

    Create Search box
    Map With Backend API.
    Add icon and update CSS.

--- academic/src/Component/StudentTable.jsx
+++ academic/src/Component/StudentTable.jsx
@@ -1,95 +1,9 @@
 import React, { useState } from "react";
-// import "./StudentTable.css"; // Optional: separate CSS file for styling
 import DeleteAlert from "./DeleteAlert";
-import axios from "axios";
 
-const StudentTable = ({ students, handleUpdateStudent }) => {
+const StudentTable = ({ students , handleDeleteStudent ,searchTerm, setSearchTerm }) => {
   const [showAlert, setShowAlert] = useState(false); // State to show/hide the alert
   const [studentToDelete, setStudentToDelete] = useState(null); // Store the student to delete
-
-  // 10 Dummy students for testing
-  const [student, setStudent] = useState([
-      {
-      id: 1,
-      name: "John Doe",
-      email: "john.doe@example.com",
-      combinationNo: "26",
-      subjectArea: ("Computer Science", "Mathematics"),
-    },
-    {
-      id: 2,
-      name: "Jane Smith",
-      email: "jane.smith@example.com",
-      combinationNo: "21",
-      subjectArea: "Mathematics",
-    },
-    {
-      id: 3,
-      name: "Alice Johnson",
-      email: "alice.johnson@example.com",
-      combinationNo: "10",
-      subjectArea: "Physics",
-    },
-    {
-      id: 4,
-      name: "Bob Brown",
-      email: "bob.brown@example.com",
-      combinationNo: "31",
-      subjectArea: "Chemistry",
-    },
-    {
-      id: 5,
-      name: "Charlie Lee",
-      email: "charlie.lee@example.com",
-      combinationNo: "28",
-      subjectArea: "Biology",
-    },
-    {
-      id: 6,
-      name: "David Kim",
-      email: "david.kim@example.com",
-      combinationNo: "13",
-      subjectArea: "Philosophy",
-    },
-    {
-      id: 7,
-      name: "Eva White",
-      email: "eva.white@example.com",
-      combinationNo: "22",
-      subjectArea: "Economics",
-    },
-    {
-      id: 8,
-      name: "Frank Green",
-      email: "frank.green@example.com",
-      combinationNo: "01",
-      subjectArea: "History",
-    },
-    {
-      id: 9,
-      name: "Grace Harris",
-      email: "grace.harris@example.com",
-      combinationNo: "26",
-      subjectArea: "Literature",
-    },
-    {
-      id: 10,
-      name: "Henry Adams",
-      email: "henry.adams@example.com",
-      combinationNo: "26",
-      subjectArea: "Sociology",
-    },
-  ]);
-
-  const handleDeleteStudent = async (id) => {
-    try {
-      await axios.delete(`/api/students/${id}`); // Connect to real API here
-      setStudent(students.filter((student) => student.id !== id)); // Update the students state
-      setShowAlert(false); // Close the alert after deletion
-    } catch (error) {
-      console.error("Error deleting student:", error);
-    }
-  };
 
   // Show the confirmation alert
   const confirmDelete = (id) => {
@@ -105,29 +19,38 @@ const StudentTable = ({ students, handleUpdateStudent }) => {
 
   return (
     <section className="table-section">
-      <h2>Students</h2>
-      <table className="data-table" style={{}}>
-        <thead style={{ backgroundColor: "aqua" }}>
+      <div style={{display:"flex" , justifyContent:"space-between", alignItems:"center", marginBottom:'10px'}}>
+      <h2 style={{fontFamily:'monospace', fontSize:'28px'}}>Student Details</h2>
+      <input
+          type="text"
+          placeholder="Search students..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          style={{padding:'5px' , fontSize:'14px', width:'35%' , height: '30px',borderRadius:'12px',border:'1px solid #ccc' }}
+        />
+      </div>
+      <table className="data-table">
+        <thead>
           <tr>
             <th>Name</th>
             <th>Email</th>
             <th>Combination No</th>
-            <th>Subject Areas</th>
             <th>Actions</th>
           </tr>
         </thead>
         <tbody>
-          {student.map((student) => (
+        {students
+            .filter((student) =>
+              student.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+              student.email.toLowerCase().includes(searchTerm.toLowerCase())
+            ).map((student) => (
             <tr key={student.id}>
               <td>{student.name}</td>
               <td>{student.email}</td>
               <td>{student.combinationNo}</td>
-              <td>{student.subjectArea}</td>
-              <td style={{ textAlign: "center" }}>
+              <td>
                 {/* <button onClick={() => handleUpdateStudent(student.id)}>Update</button> */}
-                <button onClick={() => confirmDelete(student.id)}>
-                  Delete
-                </button>{" "}
+                <button className="delete-button" onClick={() => confirmDelete(student.id)}>Delete</button>
               </td>
             </tr>
           ))}
@@ -136,7 +59,9 @@ const StudentTable = ({ students, handleUpdateStudent }) => {
       <DeleteAlert
         isOpen={showAlert}
         message="Are you sure you want to delete this student?"
-        onConfirm={() => handleDeleteStudent(studentToDelete)}
+        onConfirm={() =>{ handleDeleteStudent(studentToDelete)
+            setShowAlert(false)
+        }}
         onCancel={cancelDelete}
       />
     </section>
--- academic/src/Pages/AdminDashboard.jsx
+++ academic/src/Pages/AdminDashboard.jsx
@@ -1,13 +1,49 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Route, Routes, Link } from "react-router-dom";
+import {
+  Route,
+  Routes,
+  Link,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
+import {
+  FaTachometerAlt,
+  FaUsers,
+  FaChalkboardTeacher,
+  FaSignOutAlt,
+} from "react-icons/fa";
 import StudentTable from "../Component/StudentTable";
 import AdvisorTable from "../Component/AdvisorTable";
+import Search from "../Component/Search";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
 import "./AdminDashboard.css";
+
+// Register Chart.js components
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const AdminDashboard = () => {
   const [advisors, setAdvisors] = useState([]);
   const [students, setStudents] = useState([]);
+  const location = useLocation(); // Track current route
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     fetchAdvisors();
@@ -16,8 +52,17 @@ const AdminDashboard = () => {
 
   const fetchAdvisors = async () => {
     try {
-      const response = await axios.get("/api/advisors"); // Update with real API
-      setAdvisors(response.data);
+      const response = await axios.get(
+        "http://localhost:8083/api/users/advisors"
+      );
+      const filteredAdvisors = response.data.map((advisor) => ({
+        id: advisor.id,
+        name: advisor.username,
+        email: advisor.email,
+        subjectArea: advisor.subjectArea,
+        educationQualification: advisor.educationQualification,
+      }));
+      setAdvisors(filteredAdvisors);
     } catch (error) {
       console.error("Error fetching advisors:", error);
     }
@@ -25,17 +70,29 @@ const AdminDashboard = () => {
 
   const fetchStudents = async () => {
     try {
-      const response = await axios.get("/api/students"); // Update with real API
-      setStudents(response.data);
+      const response = await axios.get(
+        "http://localhost:8083/api/users/students"
+      );
+      const filteredStudents = response.data.map((students) => ({
+        id: students.id,
+        name: students.username,
+        email: students.email,
+        combinationNo: students.combinationNo,
+      }));
+      setStudents(filteredStudents);
     } catch (error) {
-      console.error("Error fetching students:", error);
+      console.error("Error fetching advisors:", error);
     }
   };
 
   const handleDeleteAdvisor = async (id) => {
     try {
-      await axios.delete(`/api/advisors/${id}`);
+      const response = await axios.delete(
+        `http://localhost:8083/api/users/deleteUser/${id}`
+      );
       setAdvisors(advisors.filter((advisor) => advisor.id !== id));
+      alert("Student deleted successfully!");
+      console.log(response.data); // Optional: Log success message
     } catch (error) {
       console.error("Error deleting advisor:", error);
     }
@@ -43,8 +100,12 @@ const AdminDashboard = () => {
 
   const handleDeleteStudent = async (id) => {
     try {
-      await axios.delete(`/api/students/${id}`);
+      const response = await axios.delete(
+        `http://localhost:8083/api/users/deleteUser/${id}`
+      );
       setStudents(students.filter((student) => student.id !== id));
+      alert("Student deleted successfully!");
+      console.log(response.data); // Optional: Log success message
     } catch (error) {
       console.error("Error deleting student:", error);
     }
@@ -54,35 +115,140 @@ const AdminDashboard = () => {
     console.log(`Update advisor with ID: ${id}`);
   };
 
-  const handleUpdateStudent = (id) => {
-    console.log(`Update student with ID: ${id}`);
+  const handleLogout = () => {
+    navigate("/login");
+  };
+
+  // Prepare data for the chart
+  const subjectAreaCounts = advisors.reduce((acc, advisor) => {
+    const subject = advisor.subjectArea || "Unknown";
+    acc[subject] = acc[subject] ? acc[subject] + 1 : 1;
+    return acc;
+  }, {});
+
+  const chartData = {
+    labels: Object.keys(subjectAreaCounts),
+    datasets: [
+      {
+        label: "Number of Advisors",
+        data: Object.values(subjectAreaCounts),
+        backgroundColor: [
+          "rgba(54, 162, 162, 0.6)",
+          "rgba(220, 39, 39, 0.6)",
+          "rgba(40, 130, 200, 0.6)",
+          "rgba(255, 165, 0, 0.6)",
+          "rgba(128, 0, 128, 0.6)",
+          "rgba(255, 140, 0, 0.6)",
+        ],
+
+        borderColor: [
+          "rgba(54, 162, 162, 1)",
+          "rgba(220, 39, 39, 1)",
+          "rgba(40, 130, 200, 1)",
+          "rgba(255, 165, 0, 1)",
+          "rgba(128, 0, 128, 1)",
+          "rgba(255, 140, 0, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false, // Allow the chart to take the height and width of the container
+    plugins: {
+      legend: {
+        display: true,
+        position: "top",
+        labels: {
+          font: {
+            size: 14,
+            family: "Arial, sans-serif",
+            weight: "bold",
+          },
+          color: "#333",
+        },
+      },
+      tooltip: {
+        backgroundColor: "rgba(0, 0, 0, 0.7)",
+        titleColor: "#fff",
+        bodyColor: "#fff",
+        borderColor: "#fff",
+        borderWidth: 1,
+      },
+    },
+    scales: {
+      y: {
+        beginAtZero: true,
+        title: {
+          display: true,
+          text: "Number of Advisors",
+          font: {
+            size: 16,
+            weight: "bold",
+          },
+          color: "#333",
+        },
+        ticks: {
+          color: "#333",
+          stepSize: 1,
+        },
+      },
+      x: {
+        title: {
+          display: true,
+          text: "Subject Areas",
+          font: {
+            size: 16,
+            weight: "bold",
+          },
+          color: "#333",
+        },
+        ticks: {
+          color: "#333",
+        },
+      },
+    },
   };
 
   return (
     <div className="admin-dashboard">
       <aside className="admin-sidebar">
         <Link to="/AdminDashboard" className="admin-button">
+          <FaTachometerAlt className="admin-icon" />
           Dashboard
         </Link>
         <Link to="/AdminDashboard/students" className="admin-button">
+          <FaUsers className="admin-icon" />
           Student Table
         </Link>
         <Link to="/AdminDashboard/advisors" className="admin-button">
+          <FaChalkboardTeacher className="admin-icon" />
           Advisor Table
         </Link>
+        <button onClick={handleLogout} className="admin-logoutbutton">
+          <FaSignOutAlt className="admin-icon" />
+          Log Out
+        </button>
       </aside>
 
       <main className="admin-content">
         <div className="admin-summary">
           <div className="admin-card">
-            <h3>No of Advisors</h3>
-            <p>{advisors.length}</p>
+            <h3 style={{ fontSize: "24px" }}>No of Advisors</h3>
+            <p style={{ fontSize: "32px" }}>{advisors.length}</p>
           </div>
           <div className="admin-card">
-            <h3>No of Students</h3>
-            <p>{students.length}</p>
+            <h3 style={{ fontSize: "24px" }}>No of Students</h3>
+            <p style={{ fontSize: "32px" }}>{students.length}</p>
           </div>
         </div>
+        {location.pathname === "/AdminDashboard" && (
+          <div className="admin-chart">
+            <h3>Advisor Count by Subject Area</h3>
+            <Bar data={chartData} options={options} />
+          </div>
+        )}
 
         <Routes>
           <Route
@@ -90,8 +256,9 @@ const AdminDashboard = () => {
             element={
               <AdvisorTable
                 advisors={advisors}
-                handleUpdateAdvisor={handleUpdateAdvisor}
                 handleDeleteAdvisor={handleDeleteAdvisor}
+                searchTerm={searchTerm}
+                setSearchTerm={setSearchTerm}
               />
             }
           />
@@ -100,8 +267,9 @@ const AdminDashboard = () => {
             element={
               <StudentTable
                 students={students}
-                handleUpdateStudent={handleUpdateStudent}
                 handleDeleteStudent={handleDeleteStudent}
+                searchTerm={searchTerm}
+                setSearchTerm={setSearchTerm}
               />
             }
           />
